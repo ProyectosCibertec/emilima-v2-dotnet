e70fbc68e069d0433a24cdc39d2af3413143a965
quispewilmer <wilmerquispecode@gmail.com>
2022-10-21 22:17:27 -0500

    Add error pages

---
+++ EmilimaV2Web/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmilimaV2Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+		public IActionResult Error401()
+		{
+			return View();
+		}
+
+		public IActionResult Error403()
+        {
+            return View();
+        }
+
+        public IActionResult Error404()
+        {
+            return View();
+        }
+    }
+}
