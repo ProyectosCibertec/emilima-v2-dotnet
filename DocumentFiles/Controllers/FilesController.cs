904535aeb89d208a654b1117d94e604bb9ad0dbf
quispewilmer <wilmerquispecode@gmail.com>
2022-11-16 01:57:31 -0500

    Add files service

---
+++ DocumentFiles/Controllers/FilesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using IO = System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentFiles.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public FilesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{filename}")]
+        public async Task<ActionResult<byte[]>> GetFile(string filename)
+        {
+            string path = Path.Combine(_configuration["FileStorage"], filename);
+
+            byte[] fileInBytes = await IO.File.ReadAllBytesAsync(path);
+
+            if (fileInBytes.Length == 0)
+            {
+                return NotFound();
+            }
+
+            return fileInBytes;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostFile(IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                string path = Path.Combine(_configuration["FileStorage"], file.FileName);
+
+                using (var stream = IO.File.Create(path))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
