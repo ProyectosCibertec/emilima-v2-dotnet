50b4ba0165b1bb2b74715cb5bd5d09662a2df765
Dennis Villagaray <dennis.villagarayg@gmail.com>
2022-11-24 18:30:21 -0500

    config hot reload

--- EmilimaV2Web/Program.cs
+++ EmilimaV2Web/Program.cs
@@ -13,17 +13,17 @@ builder.Services.AddHttpClient();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(o =>
     {
-        o.ExpireTimeSpan = TimeSpan.FromDays(1);
-        o.SlidingExpiration = true;
-        o.AccessDeniedPath = "/Error/Error401";
-        o.LoginPath = "/Login/";
-        o.LogoutPath = "/Logout/";
+      o.ExpireTimeSpan = TimeSpan.FromDays(1);
+      o.SlidingExpiration = true;
+      o.AccessDeniedPath = "/Error/Error401";
+      o.LoginPath = "/Login/";
+      o.LogoutPath = "/Logout/";
     });
 
 builder.Services.AddDbContext<EmilimaContext>(o =>
- {
-     o.UseSqlServer(builder.Configuration.GetConnectionString("Connection"));
- });
+{
+  o.UseSqlServer(builder.Configuration.GetConnectionString("Connection"));
+});
 
 // builder.Services.AddDbContext<EmilimaContext>(options =>
 //         options.UseSqlServer("name=ConnectionStrings:Connection"));
@@ -35,9 +35,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
